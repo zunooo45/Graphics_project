e087a395806560816ad45f8556f09c07140aeee3
McAravey <mcaravey@live.com>
2014-06-17 17:49:38 -0600

    Initial Commit.

---
+++ GraphicsProject/Renderer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+namespace GraphicsProject
+{
+    public class Renderer : GameWindow
+    {
+        protected override void OnLoad(EventArgs e)
+        {
+            GL.ClearColor(Color4.CornflowerBlue);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            this.SwapBuffers();
+        }
+    }
+}
